e72f20f7fb68a95b102fa3a40b388d96e557efaa
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2024-01-29 23:59:31 -0300

    feat: add header and search component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,27 @@
+import { Header } from '../_components/header'
+
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { Search } from './_components/search'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+
+        <p className="text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
+    </>
+  )
+}
---
+++ app/_components/header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import { Card, CardContent } from './ui/card'
+import { Button } from './ui/button'
+import { MenuIcon } from 'lucide-react'
+
+export function Header() {
+  return (
+    <Card>
+      <CardContent className="flex justify-between p-5 items-center">
+        <Image src="/logo.png" alt="Barbershop logo" height={22} width={120} />
+
+        <Button variant="outline" size="icon" className="h-8 w-8">
+          <MenuIcon size={18} />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
